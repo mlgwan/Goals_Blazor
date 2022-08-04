7e5df05a8b1e52b27d50fc89b34aaa25dd4531e1
Ciwan Clifford <theciwan@gmail.com>
2022-08-04 21:45:18 +0200

    Goals can be viewed and Status can be changed

---
+++ Goals/Components/Goal.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Goals.Components
+{
+    public partial class Goal
+    {
+        public Goal()
+        {
+            GoalStatus = Status.Undone;
+        }
+        Status GoalStatus { get; set; }
+
+        [Parameter]
+        public string GoalText { get; set; }
+
+        enum Status
+        {
+            Undone,
+            Started,
+            Done
+        }
+
+        private void OnStatusChanged(ChangeEventArgs e)
+        {
+            switch (e.Value.ToString())
+            {
+                case "Undone":
+                    GoalStatus = Status.Undone;
+                    break;
+                case "Started":
+                    GoalStatus = Status.Started;
+                    break;
+                case "Done":
+                    GoalStatus = Status.Done;
+                    break;
+                default:
+                    throw new Exception("Impossible GoalStatus");
+            }
+            GetStatusColor();
+        }
+
+        public string GetStatusColor()
+        {
+            switch (GoalStatus)
+            {
+                case Status.Undone:
+                    return "red";
+                case Status.Started:
+                    return "gold";
+                case Status.Done:
+                    return "green";
+                default:
+                    return "white";
+            }
+        }       
+    }
+    
+}
